8cfb414261720926a2bf9f77fb7b15202f274a58
Kendi J <kendij@Kendis-MacBook-Pro.local>
2023-08-13 23:13:26 +0300

    Login screen init

---
+++ lib/presentation/widgets/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LogIn extends StatelessWidget {
+  const LogIn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
